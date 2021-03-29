25cc7ed71103448cae9cf393aeeebd3fbda8f977
Michael Mathews <micmath@gmail.com>
2021-03-29 16:30:42 +0100

    Update lucky.js

--- lucky.js
+++ lucky.js
@@ -1,51 +1,15 @@
 function handleRequest(request) {
-  const { pathname } = new URL(request.url);
   const luckyNumber = Math.floor(Math.random() * 100) + 1;
 
-  if (pathname.startsWith("/html")) {
-    const html = '<html><p>Your lucky number is <b>' + luckyNumber + '</b>.</p></html>';
+  const jsonBody = JSON.stringify({
+    luckyNumber,
+  });
 
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
-
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      luckyNumber,
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
-
-  return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-      <p>
-        Visit <a href="/json">/json</a> for JSON response and
-        <a href="/html">/html</a> for HTML response
-      </p>
-    </body>`,
-    {
-      headers: {
-        "content-type": "text/html; charset=UTF-8",
-      },
+  return new Response(jsonBody, {
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
     },
-  );
+  });
 }
 
 addEventListener("fetch", (event) => {
